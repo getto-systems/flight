da05af85d2f3682cb4e822f1f571e7fdd37d494c
shun <shun@getto.systems>
2017-09-23 17:10:52 +0900

    fix upload

--- scripts/copy-files.sh
+++ scripts/copy-files.sh
@@ -8,5 +8,7 @@ export DOCKER_HOST=$docker_host
 result=$(/usr/local/bin/docker run --rm -v flight-work:/work -v $volume:/volume busybox:latest /bin/sh -c "cp -a /work/volumes/$volume /volume/app; chown -R 1000:1000 /volume/app")
 code=$?
 
+rm -rf /work/volumes/$volume
+
 result=$(echo "$result" | base64)
 echo '{"result": "'$result'", "code": '$code'}'
